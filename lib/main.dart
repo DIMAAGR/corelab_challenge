996b60a7c7e858d2292c2d6bbbe629d9722f3928
Italo Matos <italomatossouza@hotmail.com>
2024-07-30 00:00:05 -0300

    [FIX] main import corrections

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:corelab_app_challenge/core/app/main_app.dart';
-import 'package:corelab_app_challenge/modules/main_module.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'core/app/main_app.dart';
+import 'modules/main_module.dart';
+
 void main() {
-  runApp(ModularApp(module: MainModule(), child: const MainApp()))
+  runApp(ModularApp(module: MainModule(), child: const MainApp()));
 }
