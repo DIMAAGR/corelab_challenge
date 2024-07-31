3f6cc65f7c5a99c0098dc91e3e8b6af2858ca167
Italo Matos <italomatossouza@hotmail.com>
2024-07-31 10:06:10 -0300

    [FIX] incorrect categorie module

--- lib/core/app/main_app.dart
+++ lib/core/app/main_app.dart
@@ -1,3 +1,4 @@
+import 'package:corelab_challenge/modules/shared/pages/styles/theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -10,7 +11,8 @@ class MainApp extends StatelessWidget {
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
       title: 'Corelab Challenge',
-      
+      theme: AppThemeData.theme,
+      themeMode: ThemeMode.light,
     );
   }
 }
