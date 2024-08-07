8d3d13f0fb1a66ff7976a76a49ebfef00c91bc61
Italo Matos <italomatossouza@hotmail.com>
2024-08-07 01:49:31 -0300

    [CHANGE] MenuPages to just Pages
    The modification above was made to improve the reuse of the Pages resource. By changing the name to a more generic one, it becomes much easier to reuse it, as is done within the searcher.

--- lib/modules/shared/domain/enums/pages.dart
+++ lib/modules/shared/domain/enums/pages.dart
@@ -4,11 +4,13 @@ enum Pages {
   announce('/announce/'),
   favorites('/favorites/'),
   account('/account/'),
+  search('/search/'),
   none('');
 
   final String _path;
 
   const Pages(this._path);
+
   @override
   String toString() => _path;
 }
