ba8b1ffef043ce706b8642a2cef7ac23358c1f29
Italo Matos <italomatossouza@hotmail.com>
2024-08-07 01:47:12 -0300

    [ADD] new themes data

--- lib/modules/shared/pages/styles/text_styles.dart
+++ lib/modules/shared/pages/styles/text_styles.dart
@@ -37,4 +37,81 @@ class AppTextStyle {
         fontSize: 20,
         color: AppColors.richBlack,
       );
+
+  static TextStyle get appbarSearcherField => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+        color: AppColors.cadetGrey,
+      );
+
+  static TextStyle get historyTileTitle => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+        color: AppColors.grey,
+      );
+
+  static TextStyle get recentlySearches => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 13,
+        color: AppColors.richBlack,
+      );
+
+  static TextStyle get productTitle => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 15,
+        color: AppColors.richBlack,
+      );
+
+  static TextStyle get productPrice => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 20,
+        color: AppColors.richBlack,
+      );
+
+  static TextStyle get installmentValue => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 14,
+        color: AppColors.primary,
+      );
+
+  static TextStyle get productPriceDiscounted => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 13,
+        decoration: TextDecoration.lineThrough,
+        decorationColor: AppColors.grey,
+        color: AppColors.grey,
+      );
+  static TextStyle get productCondiction => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 12,
+        color: AppColors.grey,
+      );
+  static TextStyle get productsLength => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 14,
+        color: AppColors.richBlack,
+      );
+
+  static TextStyle get percentageOFF => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 9,
+        color: Colors.white,
+      );
+
+  static TextStyle get filter => const TextStyle(
+        fontFamily: 'Dm Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 13,
+        color: Colors.white,
+      );
 }
--- lib/modules/shared/pages/styles/theme_data.dart
+++ lib/modules/shared/pages/styles/theme_data.dart
@@ -5,6 +5,7 @@ class AppThemeData {
   static get theme => ThemeData(
         primaryColor: AppColors.primary,
         scaffoldBackgroundColor: AppColors.background,
+        progressIndicatorTheme: const ProgressIndicatorThemeData(color: AppColors.primary),
         appBarTheme: const AppBarTheme(color: AppColors.primary, elevation: 0),
       );
 }
