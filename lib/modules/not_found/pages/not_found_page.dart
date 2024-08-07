37bd78162381b3eaa4802141ea032285274de804
Italo Matos <italomatossouza@hotmail.com>
2024-08-07 01:50:35 -0300

    [ADD] log message

--- lib/modules/not_found/pages/not_found_page.dart
+++ lib/modules/not_found/pages/not_found_page.dart
@@ -1,7 +1,8 @@
 import 'package:corelab_challenge/modules/shared/pages/widgets/not_found_message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-import '../../shared/domain/enums/menu_page.dart';
+import '../../shared/domain/enums/pages.dart';
 import '../../shared/pages/widgets/inferior_menu.dart';
 
 class NotFoundPage extends StatelessWidget {
@@ -9,10 +10,17 @@ class NotFoundPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    debugPrint('------------------------------------------------');
+    debugPrint('[LOG][NOT_FOUND] OPS! Parece que você caiu aqui!');
+    debugPrint('[LOG][NOT_FOUND] logo abaixo estão algumas\n' 
+               'informações que parecem interessantes!\n');
+    debugPrint('[LOG][NOT_FOUND] path: ${Modular.to.path}');
+    debugPrint('------------------------------------------------');
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(foregroundColor: Colors.white),
       body: const Center(child: NotFoundMessage()),
-      bottomNavigationBar: const InferiorMenu(page: MenuPage.none),
+      bottomNavigationBar: const InferiorMenu(page: Pages.none),
     );
   }
 }
